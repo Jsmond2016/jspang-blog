1ca7b48ed6f96c37f0cbd3fb22caddc05450fc4f
Jsmond2016 <Jsmond2016@gmail.com>
2020-11-26 16:02:30 +0800

    初步完成博客页面

---
+++ pages/components/Footer.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Date: 2020-11-26 14:59:18
+ * @Author: Jsmond2016 <jsmond2016@gmail.com>
+ * @Copyright: Copyright (c) 2020, Jsmond2016
+ */
+
+import '../../static/style/components/footer.css'
+const Footer = ()=>(
+    <div className="footer-div">
+        <div>系统由 React+Node+Ant Desgin驱动 </div>
+        <div>JSPang.com</div>
+    </div>
+)
+
+export default Footer
